acc04c25369b3dff9654c9afa44c50d52efc4b20
yjm <yjm@tenmind.com>
2017-10-30 11:29:35 +0800

    first test release

---
+++ mydepot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydepot::Application.config.secret_token = 'a81c4a01a112993574b3ff5c11bfdf1f530c27630cdec98bc9875327988f5defe2cfa46402d8ee02993c62a7d3a34e02c656c7cc85930c11f5e6c333d7c2049c'
---
+++ mytest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytest::Application.config.secret_token = '6de01098d7761a936abfe22ea339bfa492db03ccdb565f2dc9ab96a7257c41285b4408115aaa5ec95fc81d14330200504983fae3c36e55ff782c8a95b3dfc761'
